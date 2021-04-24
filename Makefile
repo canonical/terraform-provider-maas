8f98600aaf7aa1bfbe9ec69df32abbdc970e7f83
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-04-24 14:53:58 +0300

    Use golang/mock and stretchr/testify
    The framework `golang/mock` allows nicer mocking, and it comes
    with `mockgen` to generate the mocks boilerplate code.
    
    And, `stretchr/testify` comes with proper assertion helpers.

--- Makefile
+++ Makefile
@@ -38,3 +38,8 @@ install: build
 .PHONY: test
 test:
 	go test $(TEST) -v $(TESTARGS) -timeout=5m -parallel=4
+
+.PHONY: mockgen
+mockgen:
+	go install github.com/golang/mock/mockgen@v1.5.0
+	mockgen -destination ./test/mocks/gomaasapi.go github.com/juju/gomaasapi Machine,Controller
--- maas/utils_test.go
+++ maas/utils_test.go
@@ -4,106 +4,129 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+
+	mock_gomaasapi "terraform-provider-maas/test/mocks"
+
+	"github.com/golang/mock/gomock"
+	"github.com/juju/gomaasapi"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBase64Encode(t *testing.T) {
 	testCases := []struct {
-		in  []byte
-		out string
+		name string
+		in   []byte
+		out  string
 	}{
 		// normal encoding case
-		{[]byte("data should be encoded"), "ZGF0YSBzaG91bGQgYmUgZW5jb2RlZA=="},
+		{
+			name: "data is encoded",
+			in:   []byte("data should be encoded"),
+			out:  "ZGF0YSBzaG91bGQgYmUgZW5jb2RlZA==",
+		},
 		// base64 encoded input should result in no change of output
-		{[]byte("ZGF0YSBzaG91bGQgYmUgZW5jb2RlZA=="), "ZGF0YSBzaG91bGQgYmUgZW5jb2RlZA=="},
+		{
+			name: "data already encoded",
+			in:   []byte("ZGF0YSBzaG91bGQgYmUgZW5jb2RlZA=="),
+			out:  "ZGF0YSBzaG91bGQgYmUgZW5jb2RlZA==",
+		},
 	}
 
-	for _, tt := range testCases {
-		out := base64Encode(tt.in)
-		if out != tt.out {
-			t.Errorf("base64Encode(%s) => %s, want %s", tt.in, out, tt.out)
-		}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			out := base64Encode(testCase.in)
+			assert.Equal(t, testCase.out, out, fmt.Sprintf("base64Encode(%s) => %s, want %s", testCase.in, out, testCase.out))
+		})
 	}
 }
 
 func TestConvertToStringSlice(t *testing.T) {
 	testCases := []struct {
-		in  []interface{}
-		out []string
+		name string
+		in   []interface{}
+		out  []string
 	}{
-		{[]interface{}{}, []string{}},
-		{[]interface{}{"elm1", "elem2", "elem3"}, []string{"elm1", "elem2", "elem3"}},
+		{
+			name: "empty slice",
+			in:   []interface{}{},
+			out:  []string{},
+		},
+		{
+			name: "slice properly converted",
+			in:   []interface{}{"elm1", "elem2", "elem3"},
+			out:  []string{"elm1", "elem2", "elem3"},
+		},
 	}
 
-	for _, tt := range testCases {
-		out := convertToStringSlice(tt.in)
-		outType := reflect.TypeOf(out).Kind()
-		if outType != reflect.Slice {
-			t.Errorf("convertToStringSlice(%s) has type %s, expected %s", tt.in, outType, reflect.Slice)
-		}
-		for i := range out {
-			elemType := reflect.TypeOf(out[i]).Kind()
-			if elemType != reflect.String {
-				t.Errorf("convertToStringSlice(%s)[%v] has type %s, expected %s", tt.in, i, elemType, reflect.String)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			out := convertToStringSlice(testCase.in)
+			outType := reflect.TypeOf(out).Kind()
+			assert.Equal(t, reflect.Slice, outType, fmt.Sprintf("convertToStringSlice(%s) has type %s, expected %s", testCase.in, outType, reflect.Slice))
+			for i := range out {
+				elemType := reflect.TypeOf(out[i]).Kind()
+				assert.Equal(t, reflect.String, elemType, fmt.Sprintf("convertToStringSlice(%s)[%v] has type %s, expected %s", testCase.in, i, elemType, reflect.String))
 			}
-		}
+		})
 	}
 }
 
 func TestGetMaasMachine(t *testing.T) {
 	testCases := []struct {
-		ctrl MockController
-		in   string
-		out  MockMachine
-		err  error
+		name        string
+		machine_ids []string
+		in          string
+		err         error
 	}{
-		// normal case when machine is found, and returned
 		{
-			ctrl: MockController{
-				machines: []MockMachine{
-					{systemId: "id-1"},
-				},
-			},
-			in:  "id-1",
-			out: MockMachine{systemId: "id-1"},
-			err: nil,
+			name:        "machine is found",
+			machine_ids: []string{"id-1"},
+			in:          "id-1",
+			err:         nil,
 		},
-		// machine is not found
 		{
-			ctrl: MockController{
-				machines: []MockMachine{
-					{systemId: "id-1"},
-				},
-			},
-			in:  "id-4",
-			out: MockMachine{},
-			err: fmt.Errorf("machine (id-4) was not found"),
+			name:        "machine is not found",
+			machine_ids: []string{},
+			in:          "id-1",
+			err:         fmt.Errorf("machine (id-1) was not found"),
 		},
-		// multiple machines with same id are found
 		{
-			ctrl: MockController{
-				machines: []MockMachine{
-					{systemId: "id-1"},
-					{systemId: "id-1"},
-				},
-			},
-			in:  "id-1",
-			out: MockMachine{},
-			err: fmt.Errorf("multiple machines found"),
+			name:        "multiple machines found",
+			machine_ids: []string{"id-1", "id-2"},
+			in:          "id-1",
+			err:         fmt.Errorf("multiple machines found"),
 		},
 	}
 
-	for _, tt := range testCases {
-		ma, err := getMaasMachine(&tt.ctrl, tt.in)
-		if tt.err != nil {
-			if tt.err.Error() != err.Error() {
-				t.Errorf("getMaasMachine(&mockClient, %s) => err == '%s', expected err == '%s'", tt.in, err, tt.err)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+
+			var machines []gomaasapi.Machine
+			for _, id := range testCase.machine_ids {
+				machine := mock_gomaasapi.NewMockMachine(mockCtrl)
+				machine.
+					EXPECT().
+					SystemID().
+					Return(id).
+					AnyTimes()
+				machines = append(machines, machine)
 			}
-		} else if err != nil {
-			t.Errorf("getMaasMachine(&mockClient, %s) => err == '%s', expected err == nil", tt.in, err)
-		} else {
-			if ma.SystemID() != tt.out.SystemID() {
-				t.Errorf("getMaasMachine(&mockClient, %s) => machine (%s), expected machine (%s)", tt.in, ma.SystemID(), tt.out.SystemID())
+
+			client := mock_gomaasapi.NewMockController(mockCtrl)
+			client.
+				EXPECT().
+				Machines(gomock.Eq(gomaasapi.MachinesArgs{SystemIDs: []string{testCase.in}})).
+				Return(machines, nil)
+
+			ma, err := getMaasMachine(client, testCase.in)
+			if testCase.err != nil {
+				assert.Equal(t, testCase.err, err)
+			} else {
+				if assert.Nil(t, err) {
+					assert.Equal(t, testCase.in, ma.SystemID())
+				}
 			}
-		}
+		})
 	}
 }
