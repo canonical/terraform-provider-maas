53e12e35d603f04d3085298c5dbb9525977a3782
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-04-28 18:42:18 +0300

    Update maas_pod resource
    * Use `Computed` parameter flag for `Optional` when it makes sense.
    * Use `TypeSet` for `tags` parameter.
    * Add default values for `cpu_over_commit_ratio` and `memory_over_commit_ratio`.
    * Update Terraform state after Pod creation.
    * Split `getPodParams` into `getPodCreateParams` and `getPodUpdateParams`.

--- examples/maas_pod/main.tf
+++ examples/maas_pod/main.tf
@@ -19,6 +19,7 @@ resource "maas_pod" "kvm" {
   zone = "default"
   pool = "default"
   tags = [
+    "pod-console-logging",
     "virtual",
     "kvm",
   ]
@@ -26,3 +27,20 @@ resource "maas_pod" "kvm" {
   memory_over_commit_ratio = 1.5
   default_macvlan_mode = "bridge"
 }
+
+output "maas_pod_kvm-type"                              {value = maas_pod.kvm.type}
+output "maas_pod_kvm-power_address"                     {value = maas_pod.kvm.power_address}
+output "maas_pod_kvm-power_user"                        {value = maas_pod.kvm.power_user}
+output "maas_pod_kvm-name"                              {value = maas_pod.kvm.name}
+output "maas_pod_kvm-zone"                              {value = maas_pod.kvm.zone}
+output "maas_pod_kvm-pool"                              {value = maas_pod.kvm.pool}
+output "maas_pod_kvm-tags"                              {value = maas_pod.kvm.tags}
+output "maas_pod_kvm-cpu_over_commit_ratio"             {value = maas_pod.kvm.cpu_over_commit_ratio}
+output "maas_pod_kvm-memory_over_commit_ratio"          {value = maas_pod.kvm.memory_over_commit_ratio}
+output "maas_pod_kvm-default_macvlan_mode"              {value = maas_pod.kvm.default_macvlan_mode}
+output "maas_pod_kvm-resources_cores_available"         {value = maas_pod.kvm.resources_cores_available}
+output "maas_pod_kvm-resources_cores_total"             {value = maas_pod.kvm.resources_cores_total}
+output "maas_pod_kvm-resources_memory_available"        {value = maas_pod.kvm.resources_memory_available}
+output "maas_pod_kvm-resources_memory_total"            {value = maas_pod.kvm.resources_memory_total}
+output "maas_pod_kvm-resources_local_storage_available" {value = maas_pod.kvm.resources_local_storage_available}
+output "maas_pod_kvm-resources_local_storage_total"     {value = maas_pod.kvm.resources_local_storage_total}
