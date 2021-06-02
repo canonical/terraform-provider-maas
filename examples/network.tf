3585bd7f3235cc6d4949f366ff4169fa04aaca0f
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-06-02 11:58:22 +0300

    Reorganize the examples

---
+++ examples/network.tf
@@ -0,0 +1,23 @@
+data "maas_fabric" "default" {
+  name = "maas"
+}
+
+data "maas_vlan" "default" {
+  fabric_id = data.maas_fabric.default.id
+  vid = 0
+}
+
+data "maas_vlan" "vid10" {
+  fabric_id = data.maas_fabric.default.id
+  vid = 10
+}
+
+data "maas_subnet" "pxe" {
+  cidr = "10.113.1.0/24"
+  vlan_id = data.maas_vlan.default.id
+}
+
+data "maas_subnet" "vid10" {
+  cidr = "10.10.0.0/16"
+  vlan_id = data.maas_vlan.vid10.id
+}
