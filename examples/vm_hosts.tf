1515539113bec9954a42e1fbea04ed9417395b75
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-06-04 20:02:09 +0300

    Update VM host machine network and storage params
    Properly implement the network and storage parameters for
    the `maas_vm_host_machine` resource.

--- examples/vm_hosts.tf
+++ examples/vm_hosts.tf
@@ -13,7 +13,10 @@ resource "maas_vm_host_machine" "kvm" {
   vm_host = maas_vm_host.kvm.id
   cores = 1
   memory = 2048
-  storage = "disk1:15"
+
+  storage_disks {
+    size_gigabytes = 15
+  }
 }
 
 resource "maas_vm_host" "maas_machine" {
@@ -21,7 +24,35 @@ resource "maas_vm_host" "maas_machine" {
   machine = "machine-01"
 }
 
-resource "maas_vm_host_machine" "maas_machine" {
-  count = 3
+resource "maas_vm_host_machine" "maas_machine_1" {
   vm_host = maas_vm_host.maas_machine.id
+
+  network_interfaces {
+    name = "eth0"
+    subnet_cidr = data.maas_subnet.pxe.cidr
+  }
+  network_interfaces {
+    name = "eth1"
+    subnet_cidr = data.maas_subnet.vid10.cidr
+  }
+
+  storage_disks {
+    size_gigabytes = 10
+  }
+  storage_disks {
+    size_gigabytes = 15
+  }
+}
+
+resource "maas_vm_host_machine" "maas_machine_2" {
+  vm_host = maas_vm_host.maas_machine.id
+
+  network_interfaces {
+    name = "eth0"
+    ip_address = "10.113.1.107"
+  }
+
+  storage_disks {
+    size_gigabytes = 21
+  }
 }
