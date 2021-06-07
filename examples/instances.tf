b17a8f8b6181624a9f47ea1f84e27ee906a10aca
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-06-07 21:23:10 +0300

    Refactor instance allocate & deploy params

--- examples/instances.tf
+++ examples/instances.tf
@@ -1,12 +1,16 @@
 resource "maas_instance" "kvm" {
   count = 2
-  allocate_hostname = maas_vm_host_machine.kvm[count.index].hostname
-  allocate_min_cpu_count = 1
-  allocate_min_memory = 2048
-  allocate_tags = [
-    maas_tag.virtual.name,
-    maas_tag.kvm.name,
-    maas_tag.ubuntu.name,
-  ]
-  deploy_distro_series = "focal"
+  allocate_params {
+    hostname = maas_vm_host_machine.kvm[count.index].hostname
+    min_cpu_count = 1
+    min_memory = 2048
+    tags = [
+      maas_tag.virtual.name,
+      maas_tag.kvm.name,
+      maas_tag.ubuntu.name,
+    ]
+  }
+  deploy_params {
+    distro_series = "focal"
+  }
 }
