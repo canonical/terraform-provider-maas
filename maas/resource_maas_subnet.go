3e50b399d0838f8aaa70dabfd00ca67784f69c45
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-06-28 19:19:09 +0300

    Implement network managed resources
    The following network managed resources are added:
      * maas_fabric
      * maas_vlan
      * maas_subnet
      * maas_space

---
+++ maas/resource_maas_subnet.go
@@ -0,0 +1,245 @@
+package maas
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/ionutbalutoiu/gomaasclient/client"
+	"github.com/ionutbalutoiu/gomaasclient/entity"
+)
+
+func resourceMaasSubnet() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceSubnetCreate,
+		ReadContext:   resourceSubnetRead,
+		UpdateContext: resourceSubnetUpdate,
+		DeleteContext: resourceSubnetDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: func(ctx context.Context, d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+				client := m.(*client.Client)
+				subnet, err := getSubnet(client, d.Id())
+				if err != nil {
+					return nil, err
+				}
+				dnsServers := make([]string, len(subnet.DNSServers))
+				for i, ip := range subnet.DNSServers {
+					dnsServers[i] = ip.String()
+				}
+				tfState := map[string]interface{}{
+					"cidr":        subnet.CIDR,
+					"name":        subnet.Name,
+					"fabric":      subnet.VLAN.FabricID,
+					"vlan":        subnet.VLAN.VID,
+					"gateway_ip":  subnet.GatewayIP,
+					"dns_servers": dnsServers,
+					"rdns_mode":   subnet.RDNSMode,
+					"allow_dns":   subnet.AllowDNS,
+					"allow_proxy": subnet.AllowProxy,
+					"managed":     subnet.Managed,
+				}
+				for k, v := range tfState {
+					if err := d.Set(k, v); err != nil {
+						return nil, err
+					}
+				}
+				d.SetId(fmt.Sprintf("%v", subnet.ID))
+				return []*schema.ResourceData{d}, nil
+			},
+		},
+
+		Schema: map[string]*schema.Schema{
+			"cidr": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"fabric": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"vlan": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				RequiredWith: []string{"fabric"},
+			},
+			"gateway_ip": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ValidateDiagFunc: func(value interface{}, path cty.Path) diag.Diagnostics {
+					v := value.(string)
+					if ip := net.ParseIP(v); ip == nil {
+						return diag.FromErr(fmt.Errorf("gateway_ip must be a valid IP address (got '%s')", v))
+					}
+					return nil
+				},
+			},
+			"dns_servers": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+					ValidateDiagFunc: func(value interface{}, path cty.Path) diag.Diagnostics {
+						v := value.(string)
+						if ip := net.ParseIP(v); ip == nil {
+							return diag.FromErr(fmt.Errorf("dns_servers list must have valid IP addresses (found '%s' in the list)", v))
+						}
+						return nil
+					},
+				},
+			},
+			"rdns_mode": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"allow_dns": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"allow_proxy": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"managed": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
+func resourceSubnetCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+
+	params, err := getSubnetParams(client, d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	subnet, err := client.Subnets.Create(params)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(fmt.Sprintf("%v", subnet.ID))
+
+	return resourceSubnetUpdate(ctx, d, m)
+}
+
+func resourceSubnetRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	_, err = client.Subnet.Get(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceSubnetUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	params, err := getSubnetParams(client, d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	_, err = client.Subnet.Update(id, params)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceSubnetRead(ctx, d, m)
+}
+
+func resourceSubnetDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	err = client.Subnet.Delete(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func getSubnetParams(client *client.Client, d *schema.ResourceData) (*entity.SubnetParams, error) {
+	params := entity.SubnetParams{
+		CIDR:       d.Get("cidr").(string),
+		AllowDNS:   d.Get("allow_dns").(bool),
+		AllowProxy: d.Get("allow_proxy").(bool),
+		Managed:    d.Get("managed").(bool),
+	}
+	if p, ok := d.GetOk("name"); ok {
+		params.Name = p.(string)
+	}
+	if p, ok := d.GetOk("fabric"); ok {
+		fabric, err := getFabric(client, p.(string))
+		if err != nil {
+			return nil, err
+		}
+		params.Fabric = fmt.Sprintf("%v", fabric.ID)
+		if p, ok := d.GetOk("vlan"); ok {
+			vlan, err := getVlan(client, fabric.ID, p.(string))
+			if err != nil {
+				return nil, err
+			}
+			params.VLAN = fmt.Sprintf("%v", vlan.ID)
+			params.VID = vlan.VID
+		}
+	}
+	if p, ok := d.GetOk("gateway_ip"); ok {
+		params.GatewayIP = net.ParseIP(p.(string))
+	}
+	if p, ok := d.GetOk("dns_servers"); ok {
+		params.DNSServers = p.([]string)
+	}
+	if p, ok := d.GetOk("rdns_mode"); ok {
+		params.RDNSMode = p.(int)
+	}
+	return &params, nil
+}
+
+func findSubnet(client *client.Client, identifier string) (*entity.Subnet, error) {
+	subnets, err := client.Subnets.Get()
+	if err != nil {
+		return nil, err
+	}
+	for _, s := range subnets {
+		if fmt.Sprintf("%v", s.ID) == identifier || s.CIDR == identifier || s.Name == identifier {
+			return &s, nil
+		}
+	}
+	return nil, nil
+}
+
+func getSubnet(client *client.Client, identifier string) (*entity.Subnet, error) {
+	subnet, err := findSubnet(client, identifier)
+	if err != nil {
+		return nil, err
+	}
+	if subnet == nil {
+		return nil, fmt.Errorf("subnet (%s) was not found", identifier)
+	}
+	return subnet, nil
+}
