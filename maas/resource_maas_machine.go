14b03d26182508c399be615ff603aee25e731411
gregnuttall <greg.nuttall@opencredo.com>
2022-08-31 16:28:55 +0100

    refactor api client references to use maas repo name

--- maas/resource_maas_machine.go
+++ maas/resource_maas_machine.go
@@ -10,8 +10,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/ionutbalutoiu/gomaasclient/client"
-	"github.com/ionutbalutoiu/gomaasclient/entity"
+	"github.com/maas/gomaasclient/client"
+	"github.com/maas/gomaasclient/entity"
 )
 
 func resourceMaasMachine() *schema.Resource {
