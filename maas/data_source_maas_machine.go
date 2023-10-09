fcc0c858a3b53a4b46336afaa853abbd2644de47
Anton Troyanov <anton@troyanov.net>
2023-10-09 15:39:42 +0400

    fixup! Add datasources for machine and physical nic

--- maas/data_source_maas_machine.go
+++ maas/data_source_maas_machine.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/ionutbalutoiu/gomaasclient/client"
+	"github.com/maas/gomaasclient/client"
 )
 
 func dataSourceMaasMachine() *schema.Resource {
