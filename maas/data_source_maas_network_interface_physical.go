4c374bd9ca7f1de3dbce53d748f3eeb27642c017
Ian McMahon <facetious.ian@gmail.com>
2023-11-16 16:31:48 -0500

    Add datasources for machine and physical nic (#7)
    ---------
    
    Co-authored-by: Ian McMahon <facetious.ian@gmail.com>
    Co-authored-by: Anton Troyanov <anton@troyanov.net>
    Co-authored-by: Stamatis Katsaounis <stamatis.katsaounis@canonical.com>

---
+++ maas/data_source_maas_network_interface_physical.go
@@ -0,0 +1,74 @@
+package maas
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/maas/gomaasclient/client"
+)
+
+func dataSourceMaasNetworkInterfacePhysical() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceNetworkInterfacePhysicalRead,
+
+		Schema: map[string]*schema.Schema{
+			"mac_address": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The physical network interface MAC address.",
+			},
+			"machine": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The identifier (system ID, hostname, or FQDN) of the machine with the physical network interface.",
+			},
+			"mtu": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "The MTU of the physical network interface.",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The physical network interface name.",
+			},
+			"tags": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Description: "A set of tag names assigned to the physical network interface.",
+			},
+			"vlan": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "VLAN the physical network interface is connected to.",
+			},
+		},
+	}
+}
+
+func dataSourceNetworkInterfacePhysicalRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*client.Client)
+	n, err := getNetworkInterfacePhysical(client, d.Get("machine").(string), d.Get("name").(string))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	tfState := map[string]interface{}{
+		"id":          fmt.Sprintf("%v", n.ID),
+		"mac_address": n.MACAddress,
+		"machine":     d.Get("machine").(string),
+		"mtu":         n.EffectiveMTU,
+		"name":        n.Name,
+		"tags":        n.Tags,
+		"vlan":        fmt.Sprintf("%v", n.VLAN.ID),
+	}
+	if err := setTerraformState(d, tfState); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
