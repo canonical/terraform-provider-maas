ced4975f537b62cdc45b99a007c654e3eaeec0f6
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-02-03 15:22:22 +0000

    validate keyring data not set

--- maas/data_source_maas_boot_source_test.go
+++ maas/data_source_maas_boot_source_test.go
@@ -16,8 +16,9 @@ func TestAccDataSourceMaasBootSource_basic(t *testing.T) {
 	checks := []resource.TestCheckFunc{
 		resource.TestCheckResourceAttr("data.maas_boot_source.test", "url", url),
 		resource.TestCheckResourceAttrSet("data.maas_boot_source.test", "created"),
-		resource.TestCheckResourceAttrSet("data.maas_boot_source.test", "updated"),
+		resource.TestCheckNoResourceAttr("data.maas_boot_source.test", "keyring_data"),
 		resource.TestCheckResourceAttr("data.maas_boot_source.test", "keyring_filename", keyring_path),
+		resource.TestCheckResourceAttrSet("data.maas_boot_source.test", "updated"),
 	}
 
 	resource.ParallelTest(t, resource.TestCase{
