2b2259bdb0427f51543de931636c7a4db9b0c09b
Toby de Mendonça <toby.demendonca@canonical.com>
2025-04-03 22:35:27 +0100

    fix: dhcp can only be turned on when a primary rack controller is set

--- maas/resource_maas_vlan_dhcp.go
+++ maas/resource_maas_vlan_dhcp.go
@@ -186,13 +186,14 @@ func resourceVlanDHCPDelete(ctx context.Context, d *schema.ResourceData, meta in
 func getVlanDHCPParams(d *schema.ResourceData) *entity.VLANParams {
 	log.Println("running getVlanDHCPParams")
 	vlanParams := entity.VLANParams{
-		DHCPOn: true,
 	}
-
+	
 	if v, ok := d.GetOk("primary_rack_controller"); ok {
+		vlanParams.DHCPOn = true
 		vlanParams.PrimaryRack = v.(string)
 	}
 	if v, ok := d.GetOk("secondary_rack_controller"); ok {
+		vlanParams.DHCPOn = true
 		vlanParams.SecondaryRack = v.(string)
 	}
 	if v, ok := d.GetOk("relay_vlan"); ok {
