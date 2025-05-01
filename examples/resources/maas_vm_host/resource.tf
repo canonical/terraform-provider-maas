8e64049252ead7211875b28cb2d56ca699ff558c
Toby de Mendonça <toby.demendonca@canonical.com>
2025-05-01 21:03:28 +0100

    chore: add lxd vm host examples and generate docs

--- examples/resources/maas_vm_host/resource.tf
+++ examples/resources/maas_vm_host/resource.tf
@@ -7,3 +7,20 @@ resource "maas_vm_host" "kvm" {
     "kvm",
   ]
 }
+
+resource "maas_vm_host" "lxd_password" {
+  type          = "lxd"
+  power_address = "10.10.0.1"
+  lxd_project   = "test-project"
+  password      = "my-password"
+}
+
+resource "maas_vm_host" "lxd_certificate" {
+  type          = "lxd"
+  power_address = "10.10.0.1"
+  lxd_project   = "test-project"
+  certificate   = "-----BEGIN CERTIFICATE-----\n certificate-goes-here =\n-----END CERTIFICATE-----\n"
+  key           = "-----BEGIN PRIVATE KEY-----\n key-goes-here ==\n-----END PRIVATE KEY-----\n"
+}
+
+  
