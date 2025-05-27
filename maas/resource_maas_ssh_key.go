da603b0e69f017de997274d3a56e22209d266bb0
Toby de Mendonça <toby.demendonca@canonical.com>
2025-05-27 17:45:37 +0100

    chore: add create read update delete

--- maas/resource_maas_ssh_key.go
+++ maas/resource_maas_ssh_key.go
@@ -3,6 +3,7 @@ package maas
 import (
 	"context"
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/canonical/gomaasclient/entity"
@@ -15,7 +16,6 @@ func resourceMAASSSHKey() *schema.Resource {
 		Description: "Provides a resource to manage SSH keys in MAAS.",
 		CreateContext: resourceSSHKeyCreate,
 		ReadContext:   resourceSSHKeyRead,
-		UpdateContext: resourceSSHKeyUpdate,
 		DeleteContext: resourceSSHKeyDelete,
 		Importer: &schema.ResourceImporter{
 			StateContext: func(ctx context.Context, d *schema.ResourceData, meta any) ([]*schema.ResourceData, error) {
@@ -28,6 +28,7 @@ func resourceMAASSSHKey() *schema.Resource {
 				Type: 	  schema.TypeString,
 				Optional: true,
 				Computed: true, 
+				ForceNew: true,
 				Description: "A valid SSH public key. If specified, this key will be uploaded to MAAS. Otherwise this field will be computed.",
 				ExactlyOneOf: []string{"key", "keysource"},
 			},
@@ -35,8 +36,12 @@ func resourceMAASSSHKey() *schema.Resource {
 				Type: 	schema.TypeString,
 				Optional: true, 
 				Computed: true,
+				ForceNew: true,
 				ExactlyOneOf: []string{"key", "keysource"},
-				Description: "The source of the SSH key. Can be used to import a requesting user's SSH keys from a particular source for a specific user, specified in the format source:user. Valid sources include 'lp' for Launchpad and 'gh' for GitHub. E.g. 'lp:my_launchpad_username'",
+				Description: `The source of the SSH key. Can be used to import a requesting user's SSH key 
+				from a source for a specific user, specified in the format source:user. Valid sources 
+				include 'lp' for Launchpad and 'gh' for GitHub. E.g. 'lp:my_launchpad_username'. 
+				Note that if more than one key is obtained, the first key will be imported.`,
 			},
 			"resource_uri": {
 				Type:        schema.TypeString,
@@ -51,7 +56,7 @@ func resourceSSHKeyCreate(ctx context.Context, d *schema.ResourceData, meta any)
 	client := meta.(*ClientConfig).Client
 
 	keyVal, keySpecified := d.GetOk("key")
-	keysource, kesourceSpecified := d.GetOk("keysource") 
+	keysource, keysourceSpecified := d.GetOk("keysource") 
 
 	var key *entity.SSHKey
 	var err error
@@ -59,16 +64,23 @@ func resourceSSHKeyCreate(ctx context.Context, d *schema.ResourceData, meta any)
 		case keySpecified:
 			key, err = client.SSHKeys.Create(keyVal.(string))
 			if err != nil {
-				return diag.FromErr(fmt.Errorf("Error creating SSH key: %v", err))
+				return diag.FromErr(fmt.Errorf("error creating SSH key: %v", err))
 			}
-		case kesourceSpecified:
+		case keysourceSpecified:
 			// needs to be multiple.
-			key, err = client.SSHKeys.Import(keysource.(string))
+			keys, err := client.SSHKeys.Import(keysource.(string))
 			if err != nil {
-				return diag.FromErr(fmt.Errorf("Error importing SSH key from source '%s': %v", keysource, err))
+				return diag.FromErr(fmt.Errorf("error importing SSH key from source '%s': %v", keysource, err))
 			}
+			if len(keys) == 0 {
+				return diag.FromErr(fmt.Errorf("no SSH keys imported from source '%s'", keysource))
+			}
+			if len(keys) > 1 {
+				log.Printf("[WARN] Multiple SSH keys found for source '%s'. Using first key.", keysource)
+			}
+			key = &keys[0]
 		default:
-			return diag.FromErr(fmt.Errorf("Either 'key' or 'keysource' must be specified to create an SSH key"))
+			return diag.FromErr(fmt.Errorf("either 'key' or 'keysource' must be specified to create an SSH key"))
 	}
 
 	d.SetId(fmt.Sprintf("%d", key.ID))
@@ -82,12 +94,12 @@ func resourceSSHKeyRead(ctx context.Context, d *schema.ResourceData, meta any) d
 	client := meta.(*ClientConfig).Client
 	sshKeyID, err := strconv.Atoi(d.Id())
 	if err != nil {
-		return diag.FromErr(fmt.Errorf("Error getting SSH key with id: %s error: ", d.Id(), err))
+		return diag.FromErr(fmt.Errorf("error converting SSH key id to int: %v", err))
 	}
 
 	sshKey, err := client.SSHKey.Get(sshKeyID)
 	if err != nil {
-		return diag.FromErr(fmt.Errorf("Error getting SSH key with id: %s error: %v", d.Id(), err))
+		return diag.FromErr(fmt.Errorf("error getting SSH key with id: %s error: %v", d.Id(), err))
 	}
 
 	tfState := map[string]any{
@@ -100,13 +112,23 @@ func resourceSSHKeyRead(ctx context.Context, d *schema.ResourceData, meta any) d
 
 	err = setTerraformState(d, tfState)
 	if err != nil {
-		return diag.FromErr(fmt.Errorf("Error setting Terraform state for SSH key with id: %s error: %v", d.Id(), err))
+		return diag.FromErr(fmt.Errorf("error setting Terraform state for SSH key with id: %s error: %v", d.Id(), err))
 	}
 
 	return nil
 }
 
+func resourceSSHKeyDelete(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	client := meta.(*ClientConfig).Client
 
-func resourceSSHKeyUpdate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	sshKeyID, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := client.SSHKey.Delete(sshKeyID); err != nil {
+		return diag.FromErr(fmt.Errorf("failed to delete SSH key with id: %d with error: %v", sshKeyID, err))
+	}
+
 	return nil
 }
