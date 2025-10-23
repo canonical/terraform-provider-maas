4f7db8cfc13cbf1a25323d6b98904f3292be7c23
Toby de Mendonça <toby.demendonca@canonical.com>
2025-10-23 16:44:07 +0100

    chore: move check env to precheck

--- maas/testutils/testutils.go
+++ maas/testutils/testutils.go
@@ -8,18 +8,8 @@ import (
 	"github.com/Masterminds/semver/v3"
 )
 
-// functions checkEnvVarsSet and checkSemverConstraint are adapted from [terraform-provider-grafana](https://github.com/grafana/terraform-provider-grafana)
+// checkSemverConstraint was adapted from [terraform-provider-grafana](https://github.com/grafana/terraform-provider-grafana)
 // Licensed under MPL-2.0
-func checkEnvVarsSet(t *testing.T, envVars ...string) {
-	t.Helper()
-
-	for _, envVar := range envVars {
-		if os.Getenv(envVar) == "" {
-			t.Fatalf("%s must be set", envVar)
-		}
-	}
-}
-
 func checkSemverConstraint(t *testing.T, semverConstraint string) {
 	t.Helper()
 
@@ -46,6 +36,5 @@ func SkipTestIfNotMAASVersion(t *testing.T, semverConstraint string) {
 		return
 	}
 
-	checkEnvVarsSet(t, "MAAS_VERSION")
 	checkSemverConstraint(t, semverConstraint)
 }
