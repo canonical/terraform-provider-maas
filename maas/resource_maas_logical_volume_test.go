64da3f354d3efe8be67d4b508a1589dde18bdd79
Stamatis Katsaounis <stamatis.katsaounis@canonical.com>
2026-01-16 14:13:10 +0200

    fix: randomizelogical volume name to avoid acceptance test resource collision

--- maas/resource_maas_logical_volume_test.go
+++ maas/resource_maas_logical_volume_test.go
@@ -66,6 +66,7 @@ func TestAccResourceMAASLogicalVolume_formatAndMount(t *testing.T) {
 	blockDevice1Name := acctest.RandomWithPrefix("tf-lv-bd")
 	blockDevice2Name := acctest.RandomWithPrefix("tf-lv-bd")
 	volumeGroupName := acctest.RandomWithPrefix("tf-lv-vg")
+	logicalVolumeName := acctest.RandomWithPrefix("tf-lv")
 
 	// Test 1: `fs_type` not specified
 	test1FsType := ""
@@ -83,15 +84,15 @@ func TestAccResourceMAASLogicalVolume_formatAndMount(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Test 1: `fs_type` not specified
 			{
-				Config:      testAccLogicalVolume(blockDevice1Name, blockDevice2Name, volumeGroupName, machine, test1FsType, "LVM test", 5, test1MountPoint),
+				Config:      testAccLogicalVolume(blockDevice1Name, blockDevice2Name, volumeGroupName, machine, test1FsType, logicalVolumeName, 5, test1MountPoint),
 				ExpectError: regexp.MustCompile(`invalid block device mount configuration: fs_type must be specified when mount_point is set`),
 			},
 			// Test 2: `mount_point` not specified
 			{
-				Config: testAccLogicalVolume(blockDevice1Name, blockDevice2Name, volumeGroupName, machine, test2FsType, "LVM test", 5, test2MountPoint),
+				Config: testAccLogicalVolume(blockDevice1Name, blockDevice2Name, volumeGroupName, machine, test2FsType, logicalVolumeName, 5, test2MountPoint),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckLogicalVolumeExists("maas_logical_volume.test"),
-					resource.TestCheckResourceAttr("maas_logical_volume.test", "name", "LVM test"),
+					resource.TestCheckResourceAttr("maas_logical_volume.test", "name", logicalVolumeName),
 					resource.TestCheckResourceAttr("maas_logical_volume.test", "fs_type", test2FsType),
 					resource.TestCheckResourceAttr("maas_logical_volume.test", "mount_point", test2MountPoint),
 				),
