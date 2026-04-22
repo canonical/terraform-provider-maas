102fcf8bfb6e9c045267cc29c14db8bc548f6ed6
Stamatis Katsaounis <stamatis.katsaounis@canonical.com>
2026-04-22 13:54:40 +0300

    fix formatting

--- maas/resource_maas_network_interface_physical_test.go
+++ maas/resource_maas_network_interface_physical_test.go
@@ -31,9 +31,9 @@ data "maas_vlan" "default" {
 }
 
 resource "maas_subnet" "test_subnet" {
-	fabric       = maas_fabric.default.id
-	vlan         = data.maas_vlan.default.id
-	cidr         = "%s"
+    fabric       = maas_fabric.default.id
+    vlan         = data.maas_vlan.default.id
+    cidr         = "%s"
 }
 
 resource "maas_network_interface_physical" "test" {
@@ -44,13 +44,13 @@ resource "maas_network_interface_physical" "test" {
     tags        = ["tag1", "tag2"]
     vlan        = data.maas_vlan.default.id
 
-	# When a physical interface is disconnected from a VLAN, MAAS automatically deletes the fabric
-	# if the VLAN has no other interfaces or subnets attached. To prevent the fabric from being
-	# deleted before the interface is disconnected, we add an explicit dependency on the subnet.
-	# This ensures the subnet (and thus the fabric) remains until after the interface is
-	# disconnected, allowing us to verify the disconnection without the fabric being removed
-	# prematurely.
-	# https://github.com/canonical/maas/commit/885021185340f740355faf13ad17b8fde5d8d285
+    # When a physical interface is disconnected from a VLAN, MAAS automatically deletes the fabric
+    # if the VLAN has no other interfaces or subnets attached. To prevent the fabric from being
+    # deleted before the interface is disconnected, we add an explicit dependency on the subnet.
+    # This ensures the subnet (and thus the fabric) remains until after the interface is
+    # disconnected, allowing us to verify the disconnection without the fabric being removed
+    # prematurely.
+    # https://github.com/canonical/maas/commit/885021185340f740355faf13ad17b8fde5d8d285
     depends_on = [maas_subnet.test_subnet]
   }
 `, fabricName, machine, testutils.GenerateRandomCIDR(), name, macAddress, mtu)
