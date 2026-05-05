f65a3c55c3a07cd4319a1ce64fd51a998ee6b999
Skye (Jack) Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2026-05-05 10:27:00 +0100

    linting

--- maas/resource_maas_vm_host_machine.go
+++ maas/resource_maas_vm_host_machine.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/canonical/gomaasclient/entity"
-	"github.com/canonical/gomaasclient/entity/node"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
